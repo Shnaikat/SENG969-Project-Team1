3c1f8362d7603ccb36bfd863482e93e62feea3a5
Jagrit Acharya <jagritacharya2020@gmail.com>
2023-11-30 19:40:08 -0700

    sql data added

---
+++ mcrs-db/concerts.sql
@@ -0,0 +1,14 @@
+/*
+-- Query: SELECT * FROM `mcrs-db`.concerts
+LIMIT 0, 1000
+
+-- Date: 2023-11-29 12:03
+*/
+INSERT INTO `` (`concertID`,`location`,`Ticket`,`genre`) VALUES (1,'NW',20,'jazz');
+INSERT INTO `` (`concertID`,`location`,`Ticket`,`genre`) VALUES (2,'NE',80,'classic');
+INSERT INTO `` (`concertID`,`location`,`Ticket`,`genre`) VALUES (3,'North',60,'electrical');
+INSERT INTO `` (`concertID`,`location`,`Ticket`,`genre`) VALUES (4,'West',50,'pop');
+INSERT INTO `` (`concertID`,`location`,`Ticket`,`genre`) VALUES (5,'UD',40,'folk');
+INSERT INTO `` (`concertID`,`location`,`Ticket`,`genre`) VALUES (6,'UD',50,'Rock');
+INSERT INTO `` (`concertID`,`location`,`Ticket`,`genre`) VALUES (7,'UD',100,'jazz');
+INSERT INTO `` (`concertID`,`location`,`Ticket`,`genre`) VALUES (8,'UD',100,'jazz');
---
+++ mcrs-db/friends.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT * FROM `mcrs-db`.friends
+LIMIT 0, 1000
+
+-- Date: 2023-11-29 12:02
+*/
+INSERT INTO `` (`friendID`,`friendName`,`friendEmail`) VALUES (1,'Lolo','kkkk@yahoo.com');
+INSERT INTO `` (`friendID`,`friendName`,`friendEmail`) VALUES (2,'Elma','Elma@yahoo.com');
+INSERT INTO `` (`friendID`,`friendName`,`friendEmail`) VALUES (3,'Jim','Jim@gmail.com');
+INSERT INTO `` (`friendID`,`friendName`,`friendEmail`) VALUES (4,'Ayman','Ayman@outlook.com');
