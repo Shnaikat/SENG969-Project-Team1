0c367bd4deabe7e36e380b96256add589c69fd15
Shnaikat <121730462+Shnaikat@users.noreply.github.com>
2023-12-05 10:52:11 -0700

    Update VenueAgent.java

--- src/agents/VenueAgent.java
+++ src/agents/VenueAgent.java
@@ -1,3 +1,8 @@
+/*
+ * This agent is acting like the service provider. 
+ * If there is an upcoming concert he will notify/send the information of this concert to our data manager agent, so we can have this concerts information.
+ * We provide the concert information thru arguments when starting this agent, so we are having different ways (variation) in our implementation.
+ */
 package agents;
 
 import jade.core.Agent;
@@ -10,24 +15,37 @@ public class VenueAgent extends Agent {
     private String genre;
 
     protected void setup() {
-        System.out.println(getLocalName() + ": VenueAgent is ready.");
+        System.out.println(getLocalName() + ": ready .. this is instance of Venue agnet");
 
+       /* Object[] args = getArguments();
+        if (args != null && args.length == 3) {
+            concertLocation = args[0];
+            ticketPrice = Integer.parseInt((String) args[1]);
+            genre =args[2];*/
+
+        
         Object[] args = getArguments();
         if (args != null && args.length == 3) {
             concertLocation = (String) args[0];
             ticketPrice = Integer.parseInt((String) args[1]);
             genre = (String) args[2];
 
-            // Log the received arguments
-            System.out.println(getLocalName() + ": Received the arguments - Location: " + concertLocation + ", Price: " + ticketPrice + ", Genre: " + genre);
+            // For the sake of debugging and knowing what's going on we are printing out the received arguments
+            System.out.println(getLocalName() + ": received the arguments - Location: " + concertLocation + ", Price: " + ticketPrice + ", Genre: " + genre);
 
-            // Sending these details to the DataManagerAgent
+            
+            /*ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+            msg.addReceiver(new AID("DataManager", AID.ISLOCALNAME));//the data manager will be the receiver 
+            msg.setContent(concertLocation + "#" + ticketPrice + "#" + genre);//splitting the data using #
+            send(msg);*/
+            
+            // Sending these details to  dataManager Agent -> should be dataManager
             ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
-            msg.addReceiver(new AID("dataManager", AID.ISLOCALNAME));
-            msg.setContent(concertLocation + "#" + ticketPrice + "#" + genre);
+            msg.addReceiver(new AID("dataManager", AID.ISLOCALNAME));//the data manager will be the receiver 
+            msg.setContent(concertLocation + "#" + ticketPrice + "#" + genre);//splitting the data using #
             send(msg);
         } else {
-            System.out.println(getLocalName() + ": Incorrect number of arguments provided.");
+            System.out.println(getLocalName() + ": The provided No. arguments is incorrect ");
             doDelete();
         }
     }
