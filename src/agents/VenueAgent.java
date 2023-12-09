e024caee11118546897b1e2e924d2db7f638862f
Raha <76937315+raha96@users.noreply.github.com>
2023-12-08 18:44:51 -0700

    Update VenueAgent.java
    Added vintage code to the comments for archival

--- src/agents/VenueAgent.java
+++ src/agents/VenueAgent.java
@@ -54,3 +54,94 @@ public class VenueAgent extends Agent {
         }
     }
 }
+
+/* This is an earlier version of the code in which we experimented with a different agent architecture, with GUI and a different inter-agent communication structure. 
+    public class Venue extends Agent {
+	// Communication target
+	String dataManager = "DataManager";
+	JTextField txtLocation = new JTextField(15);
+	JTextField txtTicketPrice = new JTextField(15);
+	JTextField txtGenre = new JTextField(15);
+	
+	private String createMessage (String location, String ticketPrice, String genre) {
+		return location + "," + ticketPrice + "," + genre;
+	}
+	
+	private class addBtnMouseListenerClass implements MouseListener {
+		public void mouseEntered(MouseEvent e) {}
+		public void mousePressed(MouseEvent e) {}
+		public void mouseClicked(MouseEvent e) {
+			//JOptionPane.showMessageDialog(null, "Haha you clicked");
+			ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+			msg.setContent(createMessage(txtLocation.getText(), txtTicketPrice.getText(), txtGenre.getText()));
+			msg.addReceiver(new AID(dataManager, AID.ISLOCALNAME));
+			send(msg);
+		}
+		public void mouseReleased(MouseEvent e) {}
+		public void mouseExited(MouseEvent e) {}
+	}
+	
+	void createGui() {
+		JFrame frame = new JFrame("Add Concert");
+		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);  // Hehe
+		frame.setSize(300, 180);
+		
+		JPanel pnlConcert = new JPanel();
+		pnlConcert.setLayout(new BoxLayout(pnlConcert, BoxLayout.PAGE_AXIS));
+		
+		JLabel lblLocation = new JLabel("Location: ");
+		JPanel pnlLocation = new JPanel();
+		pnlLocation.add(lblLocation);
+		pnlLocation.add(txtLocation);
+		
+		JLabel lblTicketPrice = new JLabel("Ticket Price: ");
+		JPanel pnlTicketPrice = new JPanel();
+		pnlTicketPrice.add(lblTicketPrice);
+		pnlTicketPrice.add(txtTicketPrice);
+		
+		JLabel lblGenre = new JLabel("Genre: ");
+		JPanel pnlGenre = new JPanel();
+		pnlGenre.add(lblGenre);
+		pnlGenre.add(txtGenre);
+		
+		JButton btnAdd = new JButton("Add");
+		JPanel pnlAdd = new JPanel();
+		addBtnMouseListenerClass addBtnMouseListener = new addBtnMouseListenerClass();
+		btnAdd.addMouseListener(addBtnMouseListener);
+		pnlAdd.add(btnAdd);
+		
+		pnlConcert.add(pnlLocation);
+		pnlConcert.add(pnlTicketPrice);
+		pnlConcert.add(pnlGenre);
+		pnlConcert.add(pnlAdd);
+		
+		frame.add(pnlConcert);
+		frame.setVisible(true);
+	}
+	@Override
+	protected void setup() {
+		createGui();
+		addBehaviour (new CyclicBehaviour() {
+			@Override
+			public void action() {
+				ACLMessage msg = receive();
+				if (msg != null) {
+					String senderName = msg.getSender().getLocalName();
+					System.out.println("Received from " + senderName + ": " + msg.getContent());
+					if (senderName.matches(dataManager)) {
+						String content = msg.getContent();
+						if (content.matches("0"))
+							JOptionPane.showMessageDialog(null, "An error occured while adding data. ");
+						else if (content.matches("1"))
+							JOptionPane.showMessageDialog(null, "Successfully added the record to the database. ");
+						else
+							JOptionPane.showMessageDialog(null, "Unknown respose: `" + content + "`.");
+					}
+				}
+				else
+					block();
+			}
+		});
+	}
+}
+*/
